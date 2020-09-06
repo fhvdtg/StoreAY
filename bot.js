93b6a440f5a87929fb18cc2c57804f8ff34dd704
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-09-07 00:08:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -366,4 +366,37 @@ message.channel.send(embed);
 
 });
 
+client.on('raw', packet => {
+if(!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+if (packet.t == 'MESSAGE_REACTION_ADD') {
+if(packet.d.message_id == '752288614582583408') { // ايدي المسج
+let emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+if(emoji == '📧'){ // الايموجي الي بيضغط عليه عشان يسوي تكت
+let u = client.users.get(packet.d.user_id);
+let channel = client.channels.get(packet.d.channel_id);
+if(channel.type == "dm"||!channel.guild) return; // ._.
+channel.fetchMessage(packet.d.message_id).then(message => {
+let re = message.reactions.get(emoji);
+re.remove(u); // عشان بعد ما يحط الايموجي ينشال
+let CH = message.guild.channels.find(r => r.id == '752288795273199867'); // ايدي الكاتوجري الي بتنحط تحتها التكتات
+if(!CH) return;
+channel.guild.createChannel(`ticket-${u.username}`,
+{
+  type: 'text',parent:CH,reason:'Reaction Tickets System',
+  permissionOverwrites: [{
+    id:  channel.guild.id,
+    deny: ['READ_MESSAGES']
+  },{
+    id: u.id,
+    allow: ['SEND_MESSAGES','READ_MESSAGES','ATTACH_FILES','READ_MESSAGE_HISTORY']
+  },{
+    id: '622121717363638282', // ايدي رتبه السبورت
+    allow: ['SEND_MESSAGES','READ_MESSAGES','ATTACH_FILES','READ_MESSAGE_HISTORY']
+  }]
+})
+}) }
+ }
+}
+});
+
 client.login(process.env.BOT_TOKEN);
