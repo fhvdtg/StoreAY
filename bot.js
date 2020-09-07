1732d3617c4fb19e24c96d3af38e1e7b167b97af
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-09-07 11:38:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,56 +31,12 @@ console.log("1")
 console.log("====================================")
 });
           
-client.on('message', msg => {
+/**client.on('message', msg => {
        if(!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send('**لا تملك الصلاحيات الكافيه**' );
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
-});
-
- client.on('message', message => {
-    var p = message.mentions.members.first();
-    var reason = message.content.split(" ").slice(2).join(' ');
-    var log = message.guild.channels.find('name', 'ban-log'); //سوي روم اسمها ban-log
-   if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS"))
-      return message.channel.send("`You dont have BAN_MEMBERS Permission!`");
-    if(message.content.startsWith(`${prefix}ban`)){
-        if(!p) return message.reply(`**منشن الشخص**`);
-        if(!reason) return message.reply(`**حط سبب
-('1', "**نشر روابط بدون اذن الادارة**")
-('2', "**نشر في الخاص**")
-('3', "**اسم غير لائق**")
-('4', "**صوره غير لائقه**")
-('5', "**سب الاهل**")
-('6', "**سب**")
-('7', "**تقليل احترام**")
-('8', "**تحرش**")
-**`);
-        if(!p.bannable) return message.reply(`**م اقدر ابند شخص من الستاف**`);
-        reason = reason.replace('1', "**نشر روابط بدون اذن الادارة**");
-        reason = reason.replace('2', "**نشر في الخاص**");
-        reason = reason.replace('3', "**اسم غير لائق**");
-        reason = reason.replace('4', "**صوره غير لائقه**");
-        reason = reason.replace('5', "**سب الاهل**");
-        reason = reason.replace('6', "**سب**");
-        reason = reason.replace('7', "**تقليل احترام**");
-        reason = reason.replace('8', "**تحرش**");
-        reason = reason.replace
-        reason = reason.replace
-        var embed = new Discord.RichEmbed()
-        .setAuthor(`User Banned!`)
-        .addField(`Name ♣`, `<@${p.id}>`)
-        .addField(`By ♣`, `<@${message.author.id}>`)
-        .addField(`Reason ♣`, reason)
-        .setTimestamp()
-        .setColor("BLACK")
-        .setFooter(` `)
-        p.ban();
-            message.delete();
-        log.send({embed});
-       
-    }
-}); 
+});**/
 
 client.on('raw', packet => {
 if(!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
