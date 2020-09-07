40bea0493544ba8cfaea0a01a759abb784636c4d
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2020-09-07 11:35:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -149,4 +149,122 @@ channelCount.setName(`Member Count : ${membercount}.`);
  
 });
 
+ client.on("guildMemberAdd", member => {
+  let welcomer = member.guild.channels.find("name","join");
+        if(!welcomer) return;
+        if(welcomer) {
+           moment.locale('ar-ly');
+           var h = member.user;
+          let norelden = new Discord.RichEmbed()
+          .setColor('PINK')
+          .setThumbnail(h.avatarURL)
+          .setAuthor(h.username,h.avatarURL)
+          .addField('وقت انشاء حسابك بالديسكورد: ',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+           .addField('متى اخر مرة دخلت سيرفرنا: ',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true) 
+           .setFooter(`${h.tag}`,"https://cdn.discordapp.com/attachments/679390274295955467/751829922237907013/storeay.png")
+       welcomer.send({embed:norelden});          
+                 
+   
+        }
+        });
+
+client.on('message', msg => {
+  if(msg.content === '!invite')
+  msg.reply('https://discord.com/api/oauth2/authorize?client_id=752185674236559501&permissions=8&scope=bot')
+});
+
+client.on("guildMemberAdd", member => {
+    member.createDM().then(function (channel) {
+    return channel.send(`:rose: **StoreAY | Offical Discord Server,** :rose: 
+  :crown: Have fun!: ${member}:crown:  
+  You're number: ${member.guild.memberCount} 
+  **StoreAY | Offical Discord Bot,** `) 
+  }).catch(console.error)
+  })
+
+client.on('guildCreate', guild => {
+    var embed = new Discord.RichEmbed()
+    .setColor(0x5500ff)
+    .setDescription(`**Thank you for adding the bot to your server. **Bot By MrBloods**`)
+        guild.owner.send(embed)
+  });
+
+client.on('message',async message => {
+if(message.author.bot || message.channel.type === 'dm') return;
+let args = message.content.split(' ');
+let author = message.author.id;
+if(!credits[author]) credits[author] = {
+credits: 0
+}
+fs.writeFileSync("./credits.json", JSON.stringify(credits, null, 4));
+if(args[0].toLowerCase() == `${prefix}points`) {
+const mention = message.mentions.users.first() || message.author;
+const mentionn = message.mentions.users.first();
+if(!args[2]) {
+message.channel.send(`**${mention.username}, your :point:  points balance are \`$${credits[mention.id].credits}\`**`)
+} else if(mentionn && args[2]) {
+if(isNaN(args[2])) return message.channel.send(`**:x: | Error**`);
+if(args[2] < 1) return message.channel.send(`**:x: | Error**`);
+if(mention.bot) return message.channel.send(`**:x: | Error**`);      
+if(mentionn.id === message.author.id) return message.channel.send(`**:x: | Error**`);
+if(args[2] > credits[author].credits) return message.channel.send(`**:x: | Error , You Don't Have Enough Credit**`);
+if(args[2].includes("-")) return message.channel.send(`**:x: | Error**`);
+let resulting = Math.floor(args[2]-(args[2]*(5/100)));
+let tax = Math.floor(args[2]*(5/100));
+let first = Math.floor(Math.random() * 9);
+let second = Math.floor(Math.random() * 9);
+let third = Math.floor(Math.random() * 9);
+let fourth = Math.floor(Math.random() * 9);
+let num = `${first}${second}${third}${fourth}`;
+let canvas = Canvas.createCanvas(108 , 40)
+let ctx = canvas.getContext('2d');
+const background = await Canvas.loadImage("https://cdn.discordapp.com/attachments/608278049091223552/617791172810899456/hmmm.png");
+ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+ctx.font = '20px Arial Bold';
+ctx.fontSize = '20px';
+ctx.fillStyle = "#ffffff";
+message.channel.send(`**${message.author.username}, Transfer Fees: \`${tax}\`, Amount: \`$${resulting.toLocaleString()}\`**
+type these numbers to confirm: `).then(essss => {
+ctx.fillText(num, canvas.width / 2.4, canvas.height / 1.7);
+message.channel.sendFile(canvas.toBuffer()).then(m => {
+message.channel.awaitMessages(r => r.author.id === message.author.id, { max: 1, time: 20000, errors:['time'] }).then(collected => {
+if(collected.first().content === num) {
+message.channel.send(`**:moneybag: | ${message.author.username}, Done Trans \`$${resulting.toLocaleString()}\` To ${mentionn}**`);
+mention.send(`**:money_with_wings: | Transfer Receipt \`\`\`You Have Received \`$${resulting.toLocaleString()}\` From User ${message.author.username}; (ID (${message.author.id})\`\`\``);
+m.delete();
+credits[author].credits += Math.floor((-resulting.toLocaleString()));
+credits[mentionn.id].credits += Math.floor((+resulting.toLocaleString()));
+fs.writeFileSync("./credits.json", JSON.stringify(credits, null, 4));
+} else {
+m.delete();
+essss.delete();
+}
+})
+})
+})
+}else {
+message.channel.send(`**:x: | Error , Please Command True Ex: \`${prefix}money [MentionUser] [Balance]\`**`);
+}
+}
+if(args[0].toLowerCase() === `${prefix}daily`) {
+let cooldown = 8.64e+7
+let Daily = time[message.author.id]
+if (Daily !== null && cooldown - (Date.now() - Daily) > 0) {
+let times = (cooldown - (Date.now() - Daily));
+message.channel.send(`**:stopwatch: |  ${message.author.username}, your daily :point: points refreshes in ${pretty(times, {verbose:true})}.**`);
+fs.writeFile("./time.json", JSON.stringify(time), function(e) {
+if(e)throw e;
+})
+}else{
+let ammount = (300, 500, 100, 200, 120, 150, 350, 320, 220, 250);
+credits[author].credits += ammount;
+time[message.author.id] = Date.now();
+message.channel.send(`**:atm:  | ${message.author.username}, you received your :point:  ${ammount} daily points!**`);
+fs.writeFile("./credits.json", JSON.stringify(credits), function(e) {
+if(e)throw e;
+})
+}
+}
+}); // By: FireKing , Codes
+
 client.login(process.env.BOT_TOKEN);
